5d818472573c76ed085fb9578b9312a7dded4b85
d-soni <devinsoni1010@gmail.com>
2016-03-08 00:49:43 -0500

    init version of JSON creation

---
+++ createJSON.py
@@ -0,0 +1,60 @@
+import sqlite3
+import numpy
+import scipy.spatial.distance
+import json
+
+def fetchTopSubreddits(topXSkip, limit):
+    conn = sqlite3.connect(database.db)
+    subreddits = {}
+    for subreddit in con.execute("""SELECT subreddit FROM MAY2015"""):
+        if subreddit in subreddits:
+            subreddits[subreddit] += 1
+        else:
+            subreddits[subreddit] = 1
+    sortedSubs = sorted(subreddits.items(), reverse = True, key = lambda x: x[1])
+    conn.close()
+    ##do not use the $topXSkip most popular, only pick $limit subreddits
+    return dict(sortedSubs[topXSkip: limit + topXSkip])
+
+def fetchUsers():
+    conn = sqlite3.connect(database.db)
+    users = []
+    for user in con.execute("""SELECT DISTINCT author from MAY2015"""):
+        users.append(user)
+    conn.close()
+    return users
+
+def mapUserVectors(topSubreddits, users):
+    conn = sqlite.connect(database.db)
+    subredditVectors = {}
+    dbData = con.execute("""SELECT subreddit, author FROM MAY2015""")
+    for subreddit, user in dbData:
+        if subreddit in subredditVectors:
+            if subreddit in topSubreddits and user in users:
+                subredditVectors[subreddit][users.index(user)] = 1
+        else:
+            subredditVectors[subreddit] = [] * len(users)
+    con.close()
+    return subredditVectors
+
+def createVectorsMatrix(subredditVectors):
+    matrix = numpy.array(subredditVectors.values())
+    return matrix
+
+def createDistanceMatrix(subredditVectors, vectorsMatrix):
+    matrix = numpy.zeros((len(subredditVectors), len(subredditVectors)))
+    distances = pdist(vectorsMatrix, "jaccard")
+    return distances
+
+def createJSON(subredditVectors, distanceMatrix):
+    jsonDistances = {}
+    for subreddit in subredditVectors:
+        if subreddit not in jsonDistances:
+            idx = subredditVectors.keys().index(subreddit)
+            jsonDistances[subreddit] = list(distanceMatrix[idx])
+    subredditMap = {}
+    subredditMap["subreddits"] = subredditVectors.keys()
+    with open("subredditMap.json", "w") as map:
+        json.dumps(subredditMap , map)
+    with open("distanceMatrix.json", "w") as matrix:
+        json.dumps(jsonDistances, matrix)
